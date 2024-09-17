4f97399f3018ea67d113502979e4ab389120c687
0tae <choiyt3465@naver.com>
2024-09-17 17:50:09 +0900

    init

---
+++ src/main/java/com/example/githubapiserver/GithubapiserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubapiserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubapiserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubapiserver/GithubapiserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubapiserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
